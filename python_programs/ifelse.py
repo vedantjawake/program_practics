9a5ba232403534dbf69cc7f40600537603f0f546
Vedant Jawake <vedantjawake6@gmail.com>
2026-08-15 23:15:09 +0530

    If -else .

---
+++ python_programs/ifelse.py
@@ -0,0 +1,14 @@
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")    
+else:
+    print("The number is negative .")
+
+number = int(input("Enter the number: "))
+
+if number % 2 == 0:
+    print("The number is even .")
+else:
+    print("The number is odd .")
+
