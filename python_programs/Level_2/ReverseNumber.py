367d609aaeee4e1ebc42c76efddc35398da974b7
Vedant Jawake <vedantjawake6@gmail.com>
2026-08-17 23:13:19 +0530

    Reverse a Number

---
+++ python_programs/Level_2/ReverseNumber.py
@@ -0,0 +1,15 @@
+
+
+number = int(input("Enter a Number: "))
+
+reverse = 0
+
+while number > 0:
+
+    digit = number % 10
+
+    reverse = reverse * 10 + digit
+
+    number = number // 10
+
+print("Reverse Number =", reverse)
